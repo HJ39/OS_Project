efa24d0b1db56115deb9109a0a2a93234bf1776c
HJ39 <jeonghojin03@gmail.com>
2022-04-19 09:10:14 -0700

    test

--- defs.h
+++ defs.h
@@ -120,9 +120,11 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
+int		cps(void);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
+
 
 // spinlock.c
 void            acquire(struct spinlock*);
